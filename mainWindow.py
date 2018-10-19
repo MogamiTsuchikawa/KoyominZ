9ea3dd9a06ea07517d8c05efd3aee8fbef65be8d
土川敦也 <atsuyatsuchikawa@gmail.com>
2018-10-19 12:26:45 +0900

    jsonによりUIの表示を出来るようにした 全てのオブジェクトでマウスによる動作をサポートした

--- mainWindow.py
+++ mainWindow.py
@@ -1,20 +1,49 @@
-import wx
+import wx,json,copy
 btn_Move = False
+Move_Object = None
+f = open("gui.json",'r')
+ui_d = json.load(f)
+DW_panel = None
 btn =[]
-class myFrame(wx.Frame):
+class DesignWindow(wx.Frame):
+    
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title)
-        panel = wx.Panel(self, wx.ID_ANY)
-        btn.append(wx.Button(panel, -1, label="Btn", pos=(10, 10)))
+        DW_panel = wx.Panel(self, wx.ID_ANY)
+        i = 0
+        for b in ui_d["Button"]:
+            print(ui_d["Button"][b])
+            btn_b = ui_d["Button"][b]
+            btn.append(wx.Button(DW_panel,-1,label=btn_b["text"],pos=(btn_b["positionX"],btn_b["positionY"])))
+            btn[i].Bind(wx.EVT_LEFT_DOWN,self.Left_Down)
+            btn[i].Bind(wx.EVT_RIGHT_DOWN,self.Right_Down)
+            i+=1
+        
+        DW_panel.Bind(wx.EVT_MOTION,self.OnMouseMove) 
+        #btn[0].Bind(wx.EVT_BUTTON,self.btn_Clicked)
+    
+    def Left_Down(self,event):
+        global DW_panel
+        global Move_Object
+        Clicked_Object = event.GetEventObject()
+        #print(Clicked_Object)
+        if Move_Object is None:
+            Move_Object = Clicked_Object
+        elif Move_Object == Clicked_Object:
+            Move_Object = None
+            
 
-        panel.Bind(wx.EVT_MOTION,self.OnMouseMove) 
-        btn[0].Bind(wx.EVT_BUTTON,self.btn_Clicked)
+    def Right_Down(self,event):
+        Clicked_Object = event.GetEventObject()
+
     def OnMouseMove(self, event):
-        global btn_Move
+        global Move_Object
         pos = event.GetPosition()
         self.SetTitle( 'OnMouseMove' + str(pos))
-        if btn_Move:
-            btn[0].SetPosition(pos)
+        
+        if Move_Object is not None:
+            print(Move_Object)
+            Move_Object.SetPosition(pos)
     
     def btn_Clicked(self,event):
         global btn_Move
@@ -24,8 +53,14 @@ class myFrame(wx.Frame):
         else:
             btn_Move = True
             print(btn_Move)
-    
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title)
+
+        Sub_Window = DesignWindow(self,"MousePoint")
+        Sub_Window_ID = Sub_Window.Show()
 app = wx.App(False)
-frame = myFrame(None, "MouseEvents")
+frame = MainWindow(None, "MouseEvents")
 frame.Show()
+
 app.MainLoop()
