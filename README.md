40b1a0d7610782cdf7eea1b63efec453313adb16
土川敦也 <atsuyatsuchikawa@gmail.com>
2018-10-20 11:54:47 +0900

    READMEファイルを追加。

---
+++ README.md
@@ -0,0 +1,10 @@
+# About KoyominZ
+
+KoyominZはポータブルC#開発環境「C#_GO」の後継ソフト,Koyominの最新バージョンです。 
+KoyominZでは今までのWPFアプリケーションでの開発を辞め、Pythonを採用することでマルチプラットフォーム対応になっています。 
+現在はUIのデザイナーの開発を行っています。 
+
+# KoyominZ の発音
+
+KoyominZは「こよみんぜーた」と呼びます。 
+たぶん次のバージョンはKoyominZZになるんでしょう...
--- mainWindow.py
+++ mainWindow.py
@@ -1,4 +1,4 @@
-import wx,json,copy
+import wx,json,copy,asyncio,threading
 
 Move_Object = None
 f = open("gui.json",'r')
@@ -6,6 +6,7 @@ ui_d = json.load(f)
 DW_panel = None
 btn =[]
 textbox = []
+StaBar = None
 class DesignWindow(wx.Frame):
     
     def __init__(self, parent, title):
@@ -13,7 +14,7 @@ class DesignWindow(wx.Frame):
         DW_panel = wx.Panel(self, wx.ID_ANY)
         i = 0
         for b in ui_d["Button"]:
-            print(ui_d["Button"][b])
+            #print(ui_d["Button"][b])
             btn_b = ui_d["Button"][b]
             btn.append(wx.Button(DW_panel,-1,label=btn_b["text"],pos=(btn_b["positionX"],btn_b["positionY"])))
             btn[i].Bind(wx.EVT_LEFT_DOWN,self.Left_Down)
@@ -21,7 +22,7 @@ class DesignWindow(wx.Frame):
             i+=1
         i = 0
         for b in ui_d["TextBox"]:
-            print(ui_d["TextBox"][b])
+            #print(ui_d["TextBox"][b])
             textbox_b = ui_d["TextBox"][b]
             textbox.append(wx.TextCtrl(DW_panel,-1,pos=(textbox_b["positionX"],textbox_b["positionY"])))
             textbox[i].Bind(wx.EVT_LEFT_DOWN,self.Left_Down)
@@ -37,8 +38,10 @@ class DesignWindow(wx.Frame):
         #print(Clicked_Object)
         if Move_Object is None:
             Move_Object = Clicked_Object
+            #MainWindow.SetStatusText("移動中")
         elif Move_Object == Clicked_Object:
             Move_Object = None
+            #MainWindow.Show_StBar()
             
 
     def Right_Down(self,event):
@@ -50,16 +53,32 @@ class DesignWindow(wx.Frame):
         self.SetTitle( 'OnMouseMove' + str(pos))
         
         if Move_Object is not None:
-            print(Move_Object)
+            #print(Move_Object)
             Move_Object.SetPosition(pos)
     
     
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
+        def Change_StaBar():
+            global Move_Object
+            if Move_Object is not None:
+                self.SetStatusText("Yes")
+            else:
+                self.SetStatusText("Nooooo")
+            t = threading.Timer(0.1,Change_StaBar)
+            t.start()
         wx.Frame.__init__(self, parent, title=title)
-
+        panel = wx.Panel(self,wx.ID_ANY)
+        StaBar = self.CreateStatusBar()
         Sub_Window = DesignWindow(self,"MousePoint")
         Sub_Window_ID = Sub_Window.Show()
+        t = threading.Thread(target=Change_StaBar)
+        t.start()
+        #loop = asyncio.get_event_loop()
+        #loop.run_until_complete(asyncio.gather(Change_StaBar(self)))
+    
+
+        
 app = wx.App(False)
 frame = MainWindow(None, "MouseEvents")
 frame.Show()
