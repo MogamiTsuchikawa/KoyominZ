01f50967b89ec12de9b1b41697cfea38517d5d56
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2018-11-12 11:57:48 +0900

    mainWindow.pyからKoyominZ.pyにリネーム 内部処理用部分のC#のプロジェクトを追加

---
+++ convert/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            string file_path = args[0];
+            
+        }
+    }
+}
