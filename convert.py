b6e261d8d3af8f58f3841a1f9043ed31f904d9ce
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2018-11-16 11:59:15 +0900

    ｯｧｱｰｰｰｰ!!!!!! XML分解ｨｨｨｨｲｨｨ!!!!!!!

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
-import wx,json,numba
+import wx,json,numba,xmltodict
+import xml.etree.ElementTree as ET
 
 class Convert():
     def __init__(self,target_file):
@@ -6,6 +7,20 @@ class Convert():
         self.target_file = target_file
         f = open(self.target_file,'r')
         self.ui_d = json.load(f)
-        self.Convert_CS()
-    def Convert_CS():
+        f.close()
+        self.Convert_CS(target_file)
+    def Convert_CS(self,target_file):
+        target_file = target_file.rstip(".json")
+        target_file = target_file + "_F.cs"
+        sf = open(target_file,'w')
         
+        
+
+def get_CS_using(project_path,target_file,project_name):
+    f = open(project_path+"/"+project_name+".csproj",'r')
+    proj_d = ET.fromstring(f.read())
+    f.close()
+
+
+
+
