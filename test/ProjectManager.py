d9079bcc912b64cd007dbc3406c6fa0606faca29
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2018-12-17 16:18:45 +0900

    MainWindow classがあるモジュールをKoyominZからMainに変更、今後起動時に最初に読まれる物としてKoyominZという名前を付ける Manager_Window class を作成 GUIのpositionとsize項目を新しいフォーマットで実装・既にデザイナには反映済み、ただしまだMainWindowプロパティ画面での表示変更の処理はしていない。

--- test/ProjectManager.py
+++ test/ProjectManager.py
@@ -1,4 +1,41 @@
-import UI_D
+import UI_D,wx
 def Load_Proj():
     Proj_Direc = ""
     
+
+class Manager_Window(wx.Frame):
+    def __init__(self,parent):
+        wx.Frame.__init__(self,parent,title="KoyominZ Project Manager")
+        self.SetSize(960,540)
+        self.panel = wx.Panel(self,wx.ID_ANY)
+        self.New_Btn = wx.Button(self.panel,-1,label="New",pos=(10,10))
+        self.New_Btn.Bind(wx.EVT_BUTTON,self.New_Btn_Clicked)
+        self.Load_Btn = wx.Button(self.panel,-1,label="Load",pos=(10,40))
+        self.Load_Btn.Bind(wx.EVT_BUTTON,self.Load_Btn_Clicked)
+        self.Setting_Btn = wx.Button(self.panel,-1,label="Setting",pos=(10,70))
+        self.Setting_Btn.Bind(wx.EVT_BUTTON,self.Setting_Btn_Clicked)
+        self.OpenWebSite_Btn = wx.Button(self.panel,-1,label="New",pos=(10,100))
+        self.OpenWebSite_Btn.Bind(wx.EVT_BUTTON,self.OpenWebSite_Btn_Clicked)
+    def New_Btn_Clicked(self,event):
+        pass
+    def Load_Btn_Clicked(self,event):
+        self.dirname = ""
+        fo_dialog = wx.FileDialog(self, "ファイルを選択してください", self.dirname, "", "*.*", wx.FD_OPEN)
+        if fo_dialog.ShowModal() == wx.ID_OK:
+            self.filename = fo_dialog.GetFilename()
+            self.dirname = fo_dialog.GetDirectory()
+            print(self.filename)
+            print(self.dirname)
+    def Setting_Btn_Clicked(self,event):
+        pass
+    def OpenWebSite_Btn_Clicked(self,event):
+        pass
+
+def Load_Proj_XML():
+    pass
+    #projectの含んでいるファイルやターゲット環境などのビルドに必要な情報を格納
+
+app = wx.App(False)
+frame = Manager_Window(None)
+frame.Show()
+app.MainLoop()
--- test/UI_D.py
+++ test/UI_D.py
@@ -3,5 +3,6 @@ ui_d_s = {}
 
 def Set_ui_d(gui_list,proj_direc):
 	for f_name in gui_list:
-		f = open(proj_direc +"/"+ f_name + ".json",'r')
+		#f = open(proj_direc +"/"+ f_name + ".json",'r')
+		f = open(f_name + ".json",'r')
 		ui_d_s[f_name] = json.load(f)
